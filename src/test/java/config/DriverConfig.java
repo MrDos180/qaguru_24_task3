9dcf0e848f9d581b01582c8bcd12c3a3d3267863
MrDos180 <ilya180@yandex.ru>
2024-02-18 16:40:18 +0300

    внес правки

--- src/test/java/config/DriverConfig.java
+++ src/test/java/config/DriverConfig.java
@@ -5,13 +5,9 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({"classpath:config/driver.properties"})
 public interface DriverConfig extends Config{
-    @Config.Key("browser.name")
     @DefaultValue("chrome")
     String browserName();
-    @Config.Key("browser.version")
     String browserVersion();
-    @Config.Key("browser.size")
     String browserSize();
-    @Config.Key("remote.url")
     String remoteUrl();
 }
